cd5cd4bff60ec60624317754b58e2fdb6cc992df
Gidss-28 <gideon-a.salangsang@gcash.com>
2025-07-04 15:56:44 +0800

    Add: NormalFareStrategy for base fare without surcharge

--- src/main/java/org/example/NormalFareStrategy.java
+++ src/main/java/org/example/NormalFareStrategy.java
@@ -1,4 +1,14 @@
 package org.example;
 
-public class NormalFareStrategy {
+public class NormalFareStrategy implements FareStrategy {
+
+    @Override
+    public FareBreakdown calculateFare(Ride ride) {
+        double baseFare = ride.getBaseFare();
+        double distanceCost = ride.getDistance() * 10.0; // +10/km
+        double durationCost = ride.getDuration() * 2.0; // +2/min
+        double surcharge = 0.0;
+
+        return new FareBreakdown(baseFare, distanceCost, durationCost, surcharge);
+    }
 }
