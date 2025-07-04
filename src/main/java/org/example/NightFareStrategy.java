3ebe009064a2d2bd9ed1dfb617892e3e07714b62
Gidss-28 <gideon-a.salangsang@gcash.com>
2025-07-04 15:57:04 +0800

    Add: NightFareStrategy with ₱50 night surcharge

--- src/main/java/org/example/NightFareStrategy.java
+++ src/main/java/org/example/NightFareStrategy.java
@@ -1,4 +1,17 @@
 package org.example;
 
-public class NightFareStrategy {
+public class NightFareStrategy implements FareStrategy {
+
+    @Override
+    public FareBreakdown calculateFare(Ride ride) {
+        FareBreakdown normal = new NormalFareStrategy().calculateFare(ride);
+        double nightSurcharge = 30.0; // Additional 30 charge
+
+        return new FareBreakdown(
+                normal.baseFare,
+                normal.distanceCost,
+                normal.durationCost,
+                nightSurcharge
+        );
+    }
 }
